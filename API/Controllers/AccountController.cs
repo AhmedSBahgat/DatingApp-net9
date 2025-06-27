2b0eb7c23bf461f40ea288397fdcbe8cd39874a4
ahmed bahgat <ahmed.bahgat@centria.fi>
2025-06-27 21:52:32 +0300

    End Of Section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -15,26 +15,24 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     [HttpPost("register")] //account/register
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
-        if (await UserExists(registerDto.Username))
-        {
-            return BadRequest("Username is taken");
-        }
-        using var hmac = new HMACSHA512();
-        var user = new AppUser
-        {
-            UserName = registerDto.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+        return Ok(); // This line is just a placeholder. You can remove it or replace it with your logic.
+        // using var hmac = new HMACSHA512();
+        // var user = new AppUser
+        // {
+        //     UserName = registerDto.Username.ToLower(),
+        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+        //     PasswordSalt = hmac.Key
 
-        };
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+        // };
+        // context.Users.Add(user);
+        // await context.SaveChangesAsync();
 
-        return new UserDto
-        {
-            Username = user.UserName,
-            Token = tokenService.CreateToken(user)
-        };
+        // return new UserDto
+        // {
+        //     Username = user.UserName,
+        //     Token = tokenService.CreateToken(user)
+        // };
     }
 
     [HttpPost("login")]
